616590340880af80d6af7e1b20f651f823e625e7
cj <oncwnuIj9nX593ZY59VZik65lFMY@git.weixin.qq.com>
2023-02-15 02:18:03 +0800

    first commit

---
+++ handler/api/api.go
@@ -0,0 +1,17 @@
+package api
+
+import (
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/wechatgpt/wechatbot/openai"
+)
+
+func Handle(msg string) *string {
+	requestText := strings.TrimSpace(msg)
+	reply, err := openai.Completions(requestText)
+	if err != nil {
+		log.Println(err)
+	}
+	return reply
+}
---
+++ handler/telegram/telegram.go
@@ -0,0 +1,16 @@
+package telegram
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/wechatgpt/wechatbot/openai"
+	"strings"
+)
+
+func Handle(msg string) *string {
+	requestText := strings.TrimSpace(msg)
+	reply, err := openai.Completions(requestText)
+	if err != nil {
+		log.Println(err)
+	}
+	return reply
+}
